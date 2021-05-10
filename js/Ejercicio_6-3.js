5a2eefcc756d48df9d446fa44224b431b56006bb
vitropez <64587299+vitropez@users.noreply.github.com>
2021-05-10 18:53:09 +0200

    10 de Mayo

--- js/Ejercicio_6-3.js
+++ js/Ejercicio_6-3.js
@@ -1,4 +1,21 @@
 // JavaScript Document
+$(document).ready(function() {
+    $(".answer").hide();
+
+    $(".main h2").click(function () {
+        if( $(this).next(".answer").is(":hidden")){
+            $(this).next('.answer').slideDown();
+            $(this).find("img").attr("src", "img/close.png");
+
+        } else {
+            $(this).next('.answer').hide();
+            $(this).find("img").attr("src", "img/open.png");
+
+        }
+    });
+});
+
+
 
 
 
