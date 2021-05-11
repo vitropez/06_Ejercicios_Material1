180e85b759673f11084e46de894e4f89ccd88e00
vitropez <64587299+vitropez@users.noreply.github.com>
2021-05-11 12:02:00 +0200

    11 de mayo

--- js/Ejercicio_6-4.js
+++ js/Ejercicio_6-4.js
@@ -1,4 +1,16 @@
 // JavaScript Document
+$(document).ready(function() {
+    $("#nav li ul").hide();
+    $('#nav li').hover(function (){
+            $(this).find('ul').slideDown();
+            $(this).css("background-color", "blue");
+
+    },
+        function (){
+        $(this).find('ul').hide();
+        $(this).css("background-color", "#555555");
+        });
+})
 
 
 
